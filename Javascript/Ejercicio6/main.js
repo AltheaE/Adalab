e913a6d8de5111acf1ca9599f5c46af3e92dff78
AltheaE <althea.espejo@gmail.com>
2017-04-21 13:06:27 +0200

    Added error if not matched 4c
    Added else for a 3c situation.

--- Javascript/Ejercicio6/main.js
+++ Javascript/Ejercicio6/main.js
@@ -1,3 +1,15 @@
-for (var leapYear = 0; leapYear < 2050; leapYear= leapYear+4) {
-  console.log(leapYear);
+//var valuecard = true;
+//valuecard = "4c";
+//if (true) {
+//console.log(valuecard);
+//}
+//Primer intento. Pero no está comprobando nada en realidad aunque el resultado esté bien.
+
+var valuecard = "4c";
+if (valuecard[0] === "4" && valuecard[1] === "c") {
+    console.log(valuecard);
+} else {
+    if (valuecard[0] === "3" && valuecard[1] === "c") {
+        console.log("Error");
+    }
 }
