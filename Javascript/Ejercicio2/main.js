c12ffd28952ab1cabafc41bd3ddb39f290515932
AltheaE <althea.espejo@gmail.com>
2017-04-21 09:59:56 +0200

    Exercise 1&2 of JS
    Ex. 1: first version of leapyear problem
    Ex.2: first version of deck problem. Find all the even black cards.

---
+++ Javascript/Ejercicio2/main.js
@@ -0,0 +1,10 @@
+var baraja= 55;
+var joker= 3;
+var totalBaraja= (baraja - joker);
+var numPalo=4
+var palo= totalBaraja/ numPalo;
+var figuras=3
+var totalPalo= (palo - figuras);
+var negras= totalPalo/2;
+var pares= negras* 2;
+console.log(pares);
