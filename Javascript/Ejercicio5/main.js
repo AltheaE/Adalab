85589d86722490e13630d81866c72f2e6949dce6
AltheaE <althea.espejo@gmail.com>
2017-04-21 12:19:45 +0200

    Exercise 5 of JS
    Check the value and suit of a predetermined card and print the result.

---
+++ Javascript/Ejercicio5/main.js
@@ -0,0 +1,12 @@
+//var valuecard = true;
+//valuecard = "4c";
+//if (true) {
+  //console.log(valuecard);
+//}
+ //Primer intento. Pero no está comprobando nada en realidad aunque el resultado esté bien.
+
+var valuecard="4c";
+if (valuecard[0]=== "4" && valuecard[1]==="c")
+{
+  console.log(valuecard);
+}
